b585a6a36556051ff3979e4e2e0115615b073807
Perrin Samuels <perrin.samuels@gmail.com>
2018-09-20 20:19:42 -0700

    update version history

--- index.js
+++ index.js
@@ -5,8 +5,9 @@
 // 2018-??-??  One week later, the Economist Web site was upgraded and returned Javascript instead of HTML.
 // 2018-09-05  Rewritten using Node.js with Mocha/Chai/Sinon testing
 // v0.1.0      set up project structure with sample function and test suite
-// v0.2.0      fetch index page for current year and display thumbnail image url for each edition
+// v0.2.0      fetch index page for current year and display thumbnail image url for each issue
 // v0.3.0      cache index page with daily expiration
+// v0.4.0      fetch images for each issue
 
 const economist = require('./lib/economist.js');
 economist.run();
